58abd3315a57909cd9e811ef68ad0dfc9e255696
mxtqnt <anacarolinass.cc@gmail.com>
2024-12-10 11:52:26 -0300

    Feat: Fibonacci

--- core/consumer.py
+++ core/consumer.py
@@ -1,14 +1,19 @@
 import zmq
+import random
 
 context = zmq.Context()
 
 print("Connecting to hello world server…")
 socket = context.socket(zmq.REQ)
 socket.connect("tcp://localhost:5555")
+print("CLIENTEEEEEE")
 
 for request in range(10):
     print("Sending request %s …" % request)
-    socket.send(b"Hello")
+    # socket.send(b"Hello")
+    numero = random.randint(1, 15)
+    print(numero)
+    socket.send(int.to_bytes(numero, byteorder='big'))
 
     message = socket.recv()
     print("Received reply %s [ %s ]" % (request, message))
--- server/socket.py
+++ server/socket.py
@@ -4,11 +4,24 @@ import zmq
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:5555")
+print("SERVERRRRR")
+
+def fibonacci(n, memo={}):
+    if n in memo:
+        return memo[n]
+    if n <= 1:
+        return n
+    memo[n] = fibonacci(n-1, memo) + fibonacci(n-2, memo)
+    return memo[n]
 
 while True:
     message = socket.recv()
     print("Received request: %s" % message)
 
     time.sleep(1)
+    resposta = fibonacci(int.from_bytes(message, byteorder='big'))
+    print(resposta) 
 
-    socket.send(b"World")
+    print(type(resposta))
+    # socket.send(b"resposta")
+    socket.send(int.to_bytes(resposta, byteorder='big'))
